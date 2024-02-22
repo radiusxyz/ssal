a278f398aad0af6e3bd607039e9bc6bfa8ddfc6d
kanet <zombiedelah@gmail.com>
2024-02-22 18:08:31 +0900

    No longer needed

--- ssal/src/epoch_manager.rs
+++
@@ -1,9 +0,0 @@
-use ssal_core::tokio::{
-    spawn,
-    time::{sleep as async_sleep, Duration},
-};
-use ssal_database::{Database, Lock};
-
-pub fn registration_epoch_manager(database: Database) {
-    spawn(async move { async_sleep(Duration::from_secs(5)) });
-}
