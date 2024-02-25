9535ad246610fc93c9eb208995e2b823f28c26fc
kanet <zombiedelah@gmail.com>
2024-02-25 14:57:54 +0900

    leader_poller() stores the block metadata into the database

--- ssal-sequencer/src/task.rs
+++ ssal-sequencer/src/task.rs
@@ -54,13 +54,25 @@ pub fn leader_poller(
                 .await
                 .unwrap()
             {
-                database
-                    .put(
-                        &("leader", &rollup_id),
-                        &(leader_id == sequencer_id, &leader_id),
-                    )
-                    .unwrap();
-                tracing::info!("{:?}; {:?}; {:?}", leader_id, rollup_id, block_height);
+                let block_metadata_key = ("block_metadata", &rollup_id);
+                match database
+                    .get_mut::<(&'static str, &RollupId), BlockMetadata>(&block_metadata_key)
+                {
+                    Ok(mut block_metadata) => {
+                        block_metadata.update(block_height, leader_id == sequencer_id, leader_id);
+                        block_metadata.commit().unwrap();
+                    }
+                    Err(error) => {
+                        if error.is_none_type() {
+                            let block_metadata = BlockMetadata::new(
+                                block_height,
+                                leader_id == sequencer_id,
+                                leader_id,
+                            );
+                            database.put(&block_metadata_key, &block_metadata).unwrap();
+                        }
+                    }
+                }
                 break;
             }
             sleep(Duration::from_millis(100)).await;
