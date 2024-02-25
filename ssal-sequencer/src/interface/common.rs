22a62ee105ecfe803dbaeeb350c7e0cd8b843124
kanet <zombiedelah@gmail.com>
2024-02-25 19:21:46 +0900

    Implement send_transaction()

--- ssal-sequencer/src/interface/common.rs
+++ ssal-sequencer/src/interface/common.rs
@@ -13,25 +13,27 @@ impl SendTransaction {
         State(state): State<Database>,
         Json(payload): Json<Self>,
     ) -> Result<impl IntoResponse, Error> {
-        // let mut block_metadata: Lock<BlockMetadata> =
-        //     state.get_mut(&("block_metadata", &payload.rollup_id))?;
+        let mut block_metadata: Lock<BlockMetadata> =
+            state.get_mut(&("block_metadata", &payload.rollup_id))?;
 
-        // if block_metadata.is_leader() {
-        //     let block_height = block_metadata.block_height();
-        //     let tx_order = block_metadata.issue_tx_order();
-        //     state.put(
-        //         &("raw_tx", &payload.rollup_id, &block_height, &tx_order),
-        //         &payload.raw_tx,
-        //     )?;
-        //     block_metadata.commit()?;
-        //     Ok((StatusCode::OK, Json(order_commitment)))
-        // } else {
-        //     let leader_id = block_metadata.leader_id();
-        //     drop(block_metadata);
-        //     let order_commitment =
-        //         forward_transaction(leader_id, payload.rollup_id, payload.raw_tx).await?;
-        //     Ok((StatusCode::OK, Json(order_commitment)))
-        // }
-        Ok((StatusCode::OK, ()))
+        if block_metadata.is_leader() {
+            let block_height = block_metadata.block_height();
+            let tx_order = block_metadata.issue_tx_order();
+            state.put(
+                &("raw_tx", &payload.rollup_id, &block_height, &tx_order),
+                &payload.raw_tx,
+            )?;
+            block_metadata.commit()?;
+
+            let order_commitment = OrderCommitment::new(block_height, tx_order);
+            Ok((StatusCode::OK, Json(order_commitment)))
+        } else {
+            let leader_id = block_metadata.leader_id();
+            drop(block_metadata);
+
+            let order_commitment =
+                forward_transaction(leader_id, payload.rollup_id, payload.raw_tx).await?;
+            Ok((StatusCode::OK, Json(order_commitment)))
+        }
     }
 }
